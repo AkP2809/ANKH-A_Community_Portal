7ef8fc62e4519223b01ee063a3fba26304c3194e
Armaan Khan <akp.wifi@gmail.com>
2021-03-31 12:45:42 +0530

    Complain Detail Activity Design

---
+++ app/src/main/java/com/example/ankhcommunity/Activities/ComplainDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.ankhcommunity.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ankhcommunity.R;
+
+public class ComplainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain_details);
+    }
+}
